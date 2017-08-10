ece2f2d7af8421e8bebc4ea426531a95cacc1ddf
jonelet <jonelet@gmail.com>
2017-08-10 18:41:35 +0200

    Add more dependencies

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: {
+    app: './src/index.js',
+  },
+
+  devtool: 'inline-source-map',
+
+  devServer: {
+    contentBase: './dist',
+    hot: true,
+  },  
+
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      }
+    ]
+  },  
+
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+  },  
+};
